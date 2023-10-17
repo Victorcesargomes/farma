868b665c1394e0128cb8669b979a4d2d81a5babe
Victor Ribeiro <76502032+Victorcesargomes@users.noreply.github.com>
2023-10-17 16:42:34 -0300

    Update apffp.py

--- apffp.py
+++ apffp.py
@@ -1,9 +1,1 @@
-import dash
-from index import *
 
-FONT_AWESOME = ["https://use.fontawesome.com/releases/v5.10.2/css/all.css"]
-
-app = dash.Dash(__name__, external_stylesheets=FONT_AWESOME, suppress_callback_exceptions=True)
-
-app.scripts.config.serve_locally = True
-server = app.server
